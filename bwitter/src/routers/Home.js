11f9b9543336aae24d509a68a0d33422ba239d15
BYUNGSOO <eedock92@naver.com>
2021-02-07 20:14:51 +0900

    commit

--- bwitter/src/routers/Home.js
+++ bwitter/src/routers/Home.js
@@ -5,10 +5,9 @@ import Bweet from "components/Bweet";
 const Home = ({userObj}) => {
     const [bweet, setBweet] = useState("");
     const [bweets, setBweets] = useState([]);
- 
+    const [attachment, setAttachment] = useState();
     useEffect(() => {
-       
-        dbService.collection("bweets").onSnapshot(snapshot => {
+       dbService.collection("bweets").onSnapshot(snapshot => {
             const bweetArray = snapshot.docs.map(doc => ({
                 id: doc.id,
                 ...doc.data(),
@@ -31,6 +30,21 @@ const Home = ({userObj}) => {
         } = event;
         setBweet(value);
     };
+    const onFileChange = (event) => {
+        const {
+            target:{files},
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent)=> {
+            const {
+                currentTarget: {result},
+            } = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile);
+    };
+    const onClearAttachment = () => setAttachment(null)
 return (
     <div>
         <form onSubmit={onSubmit}>
@@ -40,7 +54,13 @@ return (
                 placeholder="What's on your mind?" 
                 maxLength={120} 
             />
+            <input type="file" accept="image/*" onChange={onFileChange}/>
             <input type="submit" value="Bweet" />
+            {attachment &&(
+                <div>
+                    <img src={attachment} width="50px" height="50px"/>
+                    <button onClick={onClearAttachment}>Clear</button>
+                </div>)}
         </form>
         <div>
             {bweets.map((bweet) => (
